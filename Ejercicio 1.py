ae4f1c84142f38a1637a92b8223b2368dd7aa065
Andrepepa <150764630+Andrepepa@users.noreply.github.com>
2023-11-28 03:08:01 +0000

    Subiendo ejercicio 1

---
+++ Ejercicio 1.py
@@ -0,0 +1,7 @@
+resultados = []
+for numero in range(1500, 2701):
+    if numero % 7 == 0 and numero % 5 == 0:
+        resultados.append(numero)
+print("Números divisibles por 7 y múltiplos de 5 en el rango de 1500 a 2700:")
+print(resultados)
+
